89c63ab3c03dd4645692dce633b6398bb0ac3a54
Rashim-Sunar <rashimsunar.02@gmail.com>
2024-06-08 07:14:58 -0400

    Understanding param middleware

--- Day5/Routes/moviesRouter.js
+++ Day5/Routes/moviesRouter.js
@@ -3,6 +3,15 @@ const express = require('express');
 const moviesController = require("../controllers/moviesController");
 
 const router= express.Router(); //Creating a new router
+
+//param middleware which is only executed is url contains id & value==id...
+// router.param('id',(req, res, next, value)=>{
+//     console.log("Movie ID is "+  value);
+//     next();
+// })
+
+router.param('id', moviesController.checkId);
+
 router.route("/")
     .get(moviesController.getAllMovie)
     .post(moviesController.createMovie)
--- Day5/controllers/moviesController.js
+++ Day5/controllers/moviesController.js
@@ -2,6 +2,21 @@
 const fs = require('fs');
 let movies = JSON.parse(fs.readFileSync("./movies.json",'utf-8'));
 
+//Creating param middleware to check if the mocie with given id exists?
+exports.checkId = (req, res, next, value) => {
+    console.log("Movie Id id "+value);
+    id = parseInt(value);
+    let movie = movies.find(elem => elem.id === id) //returns the element of that array which matches to the condition..
+    console.log(movie)
+    if(!movie){
+       return res.status(404).json({
+            status: "fail",
+            message: "Movie with id "+id+" is not found"
+        });
+    }
+    next();
+}
+
 //ROUTE HANDLER
 exports.getAllMovie = (req,res)=>{ 
     //Using JSON JSON formatting to send the json data
@@ -18,19 +33,18 @@ exports.getMovie = (req, res)=>{
     let id = req.params.id * 1; //Multiplying by 1 converts string in numeric value
     //Find movie based on id parameter...
     let movie = movies.find(elem => elem.id === id) //returns the element of that array which matches to the condition..
-    if(!movie){
-        res.status(404).json({
-            status: "fail",
-            message: "Movie with id "+id+" is not found"
-        });
-    }else{
-        res.status(200).json({
-            status: "success",
-            data: {
-                movie: movie
-            }
-        });
-    }
+    // if(!movie){
+    //    return res.status(404).json({
+    //         status: "fail",
+    //         message: "Movie with id "+id+" is not found"
+    //     });
+    // }
+    res.status(200).json({
+        status: "success",
+        data: {
+             movie: movie
+        }
+    });  
 }
 
 exports.createMovie = (req,res)=>{
@@ -59,12 +73,12 @@ exports.createMovie = (req,res)=>{
 exports.updateMovie = (req, res)=>{
     let id = req.params.id*1; //Converting into numeric value
     let movieToUpdate = movies.find(elem => elem.id===id);
-    if(!movieToUpdate){
-        res.status(404).json({
-            status: "fail",
-            message: "Sorry, the movie with id "+id+" deosn't exist"
-        });
-    }else{
+    // if(!movieToUpdate){
+    //     return res.status(404).json({
+    //         status: "fail",
+    //         message: "Sorry, the movie with id "+id+" deosn't exist"
+    //     });
+    // }
         let index = movies.indexOf(movieToUpdate);
         let movie = Object.assign(movieToUpdate, req.body); //if name only sent in request then the name content of the resource is only overwritten and other are returned original
         movies[index] = movie;
@@ -80,18 +94,18 @@ exports.updateMovie = (req, res)=>{
                 });
             }
         }) 
-    }
+    
 }
 
 exports.deleteMovie = (req, res)=>{
     let id = req.params.id*1; //converting into numeric value
     let movieTodelete = movies.find(elem => elem.id === id);
-    if(!movieTodelete){
-        res.status(404).json({
-            status: "fail",
-            message: "Sorry, the movie with id "+id+" not found"
-        });
-    }else{
+    // if(!movieTodelete){
+    //     return res.status(404).json({
+    //         status: "fail",
+    //         message: "Sorry, the movie with id "+id+" not found"
+    //     });
+    // }
         let index = movies.indexOf(movieTodelete);
         movies.splice(index, 1);
         fs.writeFile("./movies.json", JSON.stringify(movies),(err)=>{
@@ -107,5 +121,5 @@ exports.deleteMovie = (req, res)=>{
             }
         });
         
-    }
+    
 }
