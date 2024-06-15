54e90f526d22f6afa045c32a0641bfddc3a391f7
Rashim-Sunar <rashimsunar.02@gmail.com>
2024-06-15 19:23:49 -0400

    Sorting movies based on the field

--- Day9_CINEFLEX_PROJECT_MONGODB/controllers/moviesController.js
+++ Day9_CINEFLEX_PROJECT_MONGODB/controllers/moviesController.js
@@ -22,7 +22,7 @@ exports.getAllMovie = async(req,res)=>{
     //Filtering movies by using mongoose filtering method
     /*const movies = await Movie.find()
             .where("duration").equals(req.query.duration)
-            .where("rating").equals(req.query.rating);
+            .where("rating").gte(req.query.rating);
     */
 
     //Advance filtering of movies Eg. url--> 127.0.0.1:3000/api/v1/movies/?duration[gte]=120&rating[gte]=4.5&price[lte]=50.0
@@ -31,8 +31,24 @@ exports.getAllMovie = async(req,res)=>{
     queryString = queryString.replace(/\b(gte|gt|lte|lt)\b/g, (match)=> `$${match}`); //using regular expression
     const queryObj = JSON.parse(queryString);
     // console.log(queryObj);
-    const movies = await Movie.find(queryObj);
+    // const movies = await Movie.find(queryObj);
     // .find({duration: {$gte: 120}, rating: {$gte: 4.5}, price: {$lt: 50.0}}), mongodb find() method
+
+    //Sotring movies on the basis of field...
+    //127.0.0.1:3000/api/v1/movies/?sort=-price,rating  --> -ve sign for descending order
+    
+    let query = Movie.find(); //returns query object so that we can use mongoose query method in it like .sort().
+    
+    if(req.query.sort){
+        const sortBy = req.query.sort.split(",").join(" ");
+        // console.log(sortBy);
+        query = query.sort(sortBy); //If the sort field consists only one element
+                // query.sort('releaseYear rating'); -->if sort field consists multiple elements
+    }else{
+        query = query.sort("-createdAt"); //newly created at the top.
+    }
+    
+    const movies = await query;
 
       res.status(200).json({
         status: "success",
