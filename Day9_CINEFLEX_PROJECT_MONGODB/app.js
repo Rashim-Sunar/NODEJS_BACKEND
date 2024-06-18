a4187705c07b2e97c4de4bc0e9dce203ae92cead
Rashim-Sunar <rashimsunar.02@gmail.com>
2024-06-18 07:47:20 -0400

    Creating a default route, if none of the routes are found then it will execute

--- Day9_CINEFLEX_PROJECT_MONGODB/app.js
+++ Day9_CINEFLEX_PROJECT_MONGODB/app.js
@@ -6,6 +6,12 @@ const moviesRouter = require('./Routes/moviesRouter');
 app.use(express.json())
 
 app.use("/api/v1/movies",moviesRouter); 
-
+//Creating a default route. 
+app.all("*", (req, res, next)=>{
+    res.status(404).json({
+        status: "fail",
+        message: `Can't find ${req.originalUrl} on the server`
+    });
+});
 
 module.exports = app;
