89fca2cb30e74fc629cf85f4ac0a52529d15831a
Rashim-Sunar <rashimsunar.02@gmail.com>
2024-06-19 12:34:26 -0400

    Separating error responds for development and production errors in globalErrorHandler

--- Day9_CINEFLEX_PROJECT_MONGODB/controllers/globalErrorHandler.js
+++ Day9_CINEFLEX_PROJECT_MONGODB/controllers/globalErrorHandler.js
@@ -1,8 +1,36 @@
+//Development errors are visible to the developer
+const devError = (res, error) => {
+    res.status(error.statusCode).json({
+        status: error.status,
+        message: error.message,
+        stackTrace: error.stack,
+        error: error
+    });
+}
+
+//Production error are visible to the users..
+const productionError = (res, error) => {
+    //Only opertional errors are returned to the user but not the internal server error.
+    if(error.isOperational){
+        res.status(error.statusCode).json({
+            status: error.status,
+            message: error.message
+        });
+    }else{  //else (non-operational errror)server error has occured, so send server error.
+        res.status(500).json({
+            status: "error",
+            message: "Seems like something went wrong. Please try later!"
+        }); 
+    }
+}
+
 module.exports = (error, req, res, next)=>{
     error.statusCode = error.statusCode || 500; //500 is for internal server error..
     error.status = error.status || 'error';
-    res.status(error.statusCode).json({
-        status: error.status,
-        message: error.message
-    });
+
+    if(process.env.NODE_ENV === 'development'){
+        devError(res, error);
+    }else if(process.env.NODE_ENV === 'production'){
+       productionError(res, error);
+    }
 }
