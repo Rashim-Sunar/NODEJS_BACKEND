6e74a63ca52aa62e1074cc1d1b898adbf9c0727a
Rashim-Sunar <rashimsunar.02@gmail.com>
2024-06-21 16:08:32 -0400

    Adding validation to confirm password and encrypting password

--- Day9_CINEFLEX_PROJECT_MONGODB/controllers/authController.js
+++ Day9_CINEFLEX_PROJECT_MONGODB/controllers/authController.js
@@ -1,21 +1,13 @@
 const User = require('../models/userModel');
+const asyncErrorHandler = require('./../Utils/asyncErrorHandler');
 
 //route handler middleware...
-exports.signup = async(req, res, next)=>{
-    try{
+exports.signup = asyncErrorHandler(async(req, res, next)=>{
         const newUser = await User.create(req.body);
-
         res.status(201).json({
             status: 'success',
             data: {
                 user: newUser
             }
         })
-    }catch(err){
-        res.status(400).json({
-            status: "fail",
-            message: err.message
-        })
-    }
-  
-}
+});
--- Day9_CINEFLEX_PROJECT_MONGODB/models/userModel.js
+++ Day9_CINEFLEX_PROJECT_MONGODB/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -22,9 +23,26 @@ const userSchema = new mongoose.Schema({
 
     confirmPassword: {
         type: String,
-        required: [true, "Please confirm your password."]
+        required: [true, "Please confirm your password."],
+        //custom validator.. This validator only works for .save() & .create() method.
+        validate: {
+            validator: function(val){
+                return val===this.password;
+            },
+            message: "Password and confirm passoword doesn't match!"
+        }
     }
 });
+
+//Mongoose document middleware for encrypting password before it is saved..(after chnaging the current user passoword too)
+userSchema.pre('save', async function(next){
+    if(!this.isModified('password')) return next(); //if password not changed, no need of encryption again
+    //else..hashing the password before it is saved..
+
+    this.password = await bcrypt.hash(this.password, 12);
+    this.confirmPassword = undefined; //not storing confirmPassword in database(only for validation purpose)
+    next();
+})
 
 const User = mongoose.model('user', userSchema);
 
